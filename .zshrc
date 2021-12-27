62abb53314494db1650dd63cb0107c9324e1e191
fforelle <xdollboyx@gmail.com>
2021-12-26 23:58:20 -0300

    i'm back honey

--- .zshrc
+++ .zshrc
@@ -69,3 +69,6 @@ bindkey '^e' edit-command-line
 
 # Load zsh-syntax-highlighting; should be last.
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 2>/dev/null
+
+# Aliases
+alias ls=ls -l
--- init.el
+++ init.el
@@ -9,6 +9,10 @@
 (transient-mark-mode 1)
 (require 'org)
 
+;Treemacs
+(global-set-key [f8] 'treemacs)
+(require 'treemacs)
+
 ;Ui customization
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
@@ -30,7 +34,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(org)))
+ '(package-selected-packages
+   '(evil-org pdf-tools fzf treemacs-evil treemacs markdown-mode ht org)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
