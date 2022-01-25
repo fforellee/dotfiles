27bdc35ade45f41d04734212a5f80b8e08ede608
fforelle <xdollboyx@gmail>
2022-01-25 21:59:35 +0100

    Second commit with magit!

--- dotfiles/init.el
+++ dotfiles/init.el
@@ -1,46 +1,10 @@
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-  (straight-use-package 'use-package)
-
-(straight-use-package 'doom-themes)
-(load-theme 'doom-tokyo-night t)
-
-(straight-use-package 'doom-modeline)
-(doom-modeline-mode 1)
-
-
-(straight-use-package 'all-the-icons)
-(setq doom-modeline-major-mode-icon t)
-
-(use-package all-the-icons
-:if (display-graphic-p))
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/") ;; My Themes directory  
-(menu-bar-mode -1)
-(toggle-scroll-bar -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-
-(setq display-line-numbers-type 'relative)
-(solaire-global-mode +1)
-
 (straight-use-package 'lsp-mode)
 
-(use-package typescript-mode
-:mode "\\.ts\\'"
-:hook(typescript-mode . lsp-deferred)
-:config
-(setq typescript-indent-level 2))
+;(use-package typescript-mode
+;:mode "\\.ts\\'"
+;:hook(typescript-mode . lsp-deferred)
+;:config
+;(setq typescript-indent-level 2))
 
 (straight-use-package 'haskell-mode)
 
@@ -54,6 +18,7 @@
 
 (straight-use-package 'evil)
 (straight-use-package 'evil-org-mode)
+(setq evil-want-C-u-scroll t)
 (evil-mode 1)
 (evil-org-mode 1)
 
@@ -63,15 +28,18 @@
 (straight-use-package 'vertico)
 (vertico-mode 1)
 
-(straight-use-package 'treemacs)
+(straight-use-package 'treemacs
+(treemacs-load-theme 'all-the-icons)
+(treemacs-set-width  '25))
+
 (straight-use-package 'treemacs-evil)
 (straight-use-package 'treemacs-magit)
 (straight-use-package 'treemacs-all-the-icons)
 (global-set-key [f8] 'treemacs)
 (use-package treemacs-all-the-icons
    :after treemacs)
-(treemacs-load-theme 'all-the-icons)
-(treemacs-set-width  '25)
+;(treemacs-load-theme 'all-the-icons)
+;(treemacs-set-width  '25)
 
 (straight-use-package 'magit)
 
@@ -84,7 +52,3 @@
 (which-key-mode 1)
 
 (straight-use-package 'solaire-mode)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org"."https://orgmode.org/elpa/")
-			 ("elpa"."https://melpa.org/packages/")))
