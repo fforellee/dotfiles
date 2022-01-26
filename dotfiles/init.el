ef3f71c4856bdab77b1b09d366e41941e52ed7de
fforelle <xdollboyx@gmail>
2022-01-26 06:32:56 +0100

    Addin lsp mode to emacs

--- dotfiles/init.el
+++ dotfiles/init.el
@@ -1,11 +1,49 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+  (straight-use-package 'use-package)
+
+(straight-use-package 'doom-themes)
+(load-theme 'doom-tokyo-night t)
+
+(straight-use-package 'doom-modeline)
+(doom-modeline-mode 1)
+
+
+(straight-use-package 'all-the-icons)
+(setq doom-modeline-major-mode-icon t)
+
+(use-package all-the-icons
+:if (display-graphic-p))
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/") ;; My Themes directory  
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+(setq display-line-numbers-type 'relative)
+(solaire-global-mode +1)
+;-SRC-Hack Nerd Font Mono-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1
+
 (straight-use-package 'lsp-mode)
 (straight-use-package 'dap-mode)
+(setq dap-auto-configure-features '(sessions locals controls tooltip))
 
-;(use-package typescript-mode
-;:mode "\\.ts\\'"
-;:hook(typescript-mode . lsp-deferred)
-;:config
-;(setq typescript-indent-level 2))
+(use-package typescript-mode
+:mode "\\.ts\\'"
+:hook(typescript-mode . lsp-deferred))
+(add-hook 'typescript-mode #'lsp)
+;dap mode configuration setup
 
 (straight-use-package 'haskell-mode)
 
@@ -15,10 +53,9 @@
 
 (straight-use-package 'nix-mode)
 
-(with-eval-after-load 'lsp-mode
-(require 'dap-chrome)
-(add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
-(yas-global-mode))
+;(with-eval-after-load 'lsp-mode
+;(require 'dap-chrome)
+;(add-hook 'lsp-mode-hook ))
 
 (straight-use-package 'company-mode)
 
@@ -30,6 +67,30 @@
 
 ;(straight-use-package 'org-mode)
  (add-hook 'org-mode 'display-line-numbers-mode 1)
+
+(add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
+(use-package org-superstar
+:after org
+:hook (org-mode . org-superstar-mode)
+:config
+  (set-face-attribute 'org-superstar-header-bullet nil :inherit 'fixed-pitched :height 180)
+:custom
+;; set the leading bullet to be a space. For alignment purposes I use an em-quad space (U+2001)
+(org-superstar-headline-bullets-list '(" "))
+(org-superstar-todo-bullet-alist '(("DONE" . ?✔)
+				   ("TODO" . ?⌖)
+				   ("ISSUE" . ?)
+				   ("BRANCH" . ?)
+				   ("FORK" . ?)
+				   ("MR" . ?)
+				   ("MERGED" . ?)
+				   ("GITHUB" . ?A)
+				   ("WRITING" . ?✍)
+				   ("WRITE" . ?✍)
+				   ))
+(org-superstar-special-todo-items t)
+(org-superstar-leading-bullet "")
+)
 
 (straight-use-package 'vertico)
 (vertico-mode 1)
@@ -58,3 +119,9 @@
 (which-key-mode 1)
 
 (straight-use-package 'solaire-mode)
+
+(straight-use-package 'writeroom-mode)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org"."https://orgmode.org/elpa/")
+			 ("elpa"."https://melpa.org/packages/")))
